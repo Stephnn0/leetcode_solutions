9b4dfb54dd30319e51bb895b28c17ce07139b2d7
susshitechnologies <susshitech@outlook.com>
2024-04-19 16:53:09 -0400

    third commit

--- uniqueCombination.py
+++ uniqueCombination.py
@@ -1,3 +1,16 @@
+# Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+# Each number in candidates may only be used once in the combination.
+# Note: The solution set must not contain duplicate combinations.
+
+# Input: candidates = [10,1,2,7,6,1,5], target = 8
+# Output: 
+# [
+# [1,1,6],
+# [1,2,5],
+# [1,7],
+# [2,6]
+# ]
+
 def combinationSum2(candidates, target):
             
         candidates.sort()
@@ -29,3 +42,9 @@ def combinationSum2(candidates, target):
         backtrack(0, target, [], result)
         
         return result
+
+
+candidates = [10,1,2,7,6,1,5]
+target = 8
+
+print(combinationSum2(candidates, target))
